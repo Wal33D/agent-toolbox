df7257144d00b0380abfbfabd44cb3adcce76514
Waleed Gudah <aquataze@yahoo.com>
2024-06-18 14:41:10 -0500

    .

--- utils/capitalize.ts
+++ utils/capitalize.ts
@@ -4,5 +4,5 @@
  * @returns {string} - The capitalized string.
  */
 export const capitalize = (str: string): string => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+	return str.charAt(0).toUpperCase() + str.slice(1);
 };
